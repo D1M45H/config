fb1219f409d5df7740c698593ac3e23c14ccfd28
D1M45H <hsnomod05@gmail.com>
2024-11-27 18:41:20 +0300

    Update test_config_converter.py

--- HW3/test_config_converter.py
+++ HW3/test_config_converter.py
@@ -6,6 +6,14 @@ class TestConfigConverter(unittest.TestCase):
     def setUp(self):
         self.converter = ConfigConverter()
 
+    def test_comment_1(self):
+        self.converter.handle_constant_declaration("% test")
+        self.assertEqual(result, "test")
+
+    def test_comment_2(self):
+        self.converter.handle_constant_declaration("(comment 1\n2")
+        self.assertEqual(result, "1\n2")
+
     def test_constant_declaration(self):
         self.converter.handle_constant_declaration("let test_var = 5;")
         self.assertEqual(self.converter.constants['test_var'], 5)
@@ -15,9 +23,13 @@ class TestConfigConverter(unittest.TestCase):
         self.assertEqual(result, [[1, 2], 3])
 
     def test_expression_evaluation(self):
-        self.converter.handle_constant_declaration("let a = {+ 10 2};")
+        self.converter.handle_constant_declaration("let a = {- 10 2};")
         result = self.converter.evaluate_expression("{+ a 5}")
-        self.assertEqual(result, 17)
+        self.assertEqual(result, 13)
+
+    def test_expression_evaluation_abs(self):
+        result = self.converter.evaluate_expression("{abs -5}")
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
